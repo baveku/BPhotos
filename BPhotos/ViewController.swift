2f910a669d2aa3536b6d4324b4169f1468cc1690
vuzbach <baveku@gmail.com>
2019-11-12 14:43:09 +0700

    Initial Commit

---
+++ BPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BPhotos
+//
+//  Created by Bách veku on 11/12/19.
+//  Copyright © 2019 Bách veku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BPhotosTests/BPhotosTests.swift
@@ -0,0 +1,34 @@
+//
+//  BPhotosTests.swift
+//  BPhotosTests
+//
+//  Created by Bách veku on 11/12/19.
+//  Copyright © 2019 Bách veku. All rights reserved.
+//
+
+import XCTest
+@testable import BPhotos
+
+class BPhotosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
